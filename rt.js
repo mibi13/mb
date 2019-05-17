a234bd8d8222e23c9140db00fdbb4799e690276e
mibi13 <50749444+mibi13@users.noreply.github.com>
2019-05-18 00:57:57 +0700

    Update rt.js

--- rt.js
+++ rt.js
@@ -1,104 +1,18 @@
-'use strict';
-
-/**
- * Service Worker of ReloadTrans
- */
- 
-const cacheName = 'ReloadTrans';
-const startPage = 'https://www.reloadtrans.id';
-const offlinePage = 'https://www.reloadtrans.id';
-const filesToCache = [startPage, offlinePage];
-const neverCacheUrls = [/\/search];
-
-// Install
+importScripts('https://raw.githubusercontent.com/mibi13/mb/master/client.js');
 self.addEventListener('install', function(e) {
-	console.log('ReloadTrans service worker installation');
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			console.log('ReloadTrans service worker caching dependencies');
-			filesToCache.map(function(url) {
-				return cache.add(url).catch(function (reason) {
-					return console.log('ReloadTrans: ' + String(reason) + ' ' + url);
-				});
-			});
-		})
-	);
+e.waitUntil(
+caches.open('ramaibeud').then(function(cache) {
+return cache.addAll([
+'/'
+]);
+})
+);
 });
-
-// Activate
-self.addEventListener('activate', function(e) {
-	console.log('ReloadTrans service worker activation');
-	e.waitUntil(
-		caches.keys().then(function(keyList) {
-			return Promise.all(keyList.map(function(key) {
-				if ( key !== cacheName ) {
-					console.log('ReloadTrans old cache removed', key);
-					return caches.delete(key);
-				}
-			}));
-		})
-	);
-	return self.clients.claim();
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
+event.respondWith(
+caches.match(event.request).then(function(response) {
+return response || fetch(event.request);
+})
+);
 });
-
-// Fetch
-self.addEventListener('fetch', function(e) {
-	
-	// Return if the current request url is in the never cache list
-	if ( ! neverCacheUrls.every(checkNeverCacheList, e.request.url) ) {
-	  console.log( 'ReloadTrans: Current request is excluded from cache.' );
-	  return;
-	}
-	
-	// Return if request url protocal isn't http or https
-	if ( ! e.request.url.match(/^(http|https):\/\//i) )
-		return;
-	
-	// Return if request url is from an external domain.
-	if ( new URL(e.request.url).origin !== location.origin )
-		return;
-	
-	// For POST requests, do not use the cache. Serve offline page if offline.
-	if ( e.request.method !== 'GET' ) {
-		e.respondWith(
-			fetch(e.request).catch( function() {
-				return caches.match(offlinePage);
-			})
-		);
-		return;
-	}
-	
-	// Revving strategy
-	if ( e.request.mode === 'navigate' && navigator.onLine ) {
-		e.respondWith(
-			fetch(e.request).then(function(response) {
-				return caches.open(cacheName).then(function(cache) {
-					cache.put(e.request, response.clone());
-					return response;
-				});  
-			})
-		);
-		return;
-	}
-
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			return response || fetch(e.request).then(function(response) {
-				return caches.open(cacheName).then(function(cache) {
-					cache.put(e.request, response.clone());
-					return response;
-				});  
-			});
-		}).catch(function() {
-			return caches.match(offlinePage);
-		})
-	);
-});
-
-// Check if current url is in the neverCacheUrls list
-function checkNeverCacheList(url) {
-	if ( this.match(url) ) {
-		return false;
-	}
-	return true;
-}
